85d3eaae2d07e889fa5b262c1766fe4157c1e9c7
CrandyIsAtWork <bodenbeutler@gmail.com>
2025-08-27 14:12:51 -0600

    Added Tank Game
    Added the game code

---
+++ Prototype 1 - Tank Game 2025/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotSpeed;
+    public float hInput;
+    public float vInput;
+    public float jumpForce;
+    public Rigidbody playerRB;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, hInput * rotSpeed * Time.deltaTime); //Rotates the vehicle left and right
+        transform.Translate(Vector3.forward * vInput * speed * Time.deltaTime); //Move the vehicle forward
+    }
+}
